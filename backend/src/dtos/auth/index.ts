fc2badcd92322b463c8c2e839677c1ebc1cd5404
veltonmonte <montevelton@gmail.com>
2026-09-01 16:54:59 -0300

    feat(auth): refatorar autenticação com Service + DTOs + Prisma

---
+++ backend/src/dtos/auth/index.ts
@@ -0,0 +1,6 @@
+export * from "./register.dto.js";
+export * from "./login.dto.js";
+export * from "./update-user.dto.js";
+export * from "./user-response.dto.js";
+export * from "./auth-response.dto.js";
+export * from "./paginated-users.dto.js";
